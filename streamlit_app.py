719978be223b9e5792af6b07a6aef141321e5763
kundan888 <114283397+kundan888@users.noreply.github.com>
2024-03-24 13:00:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,34 +13,28 @@ with open("roadsylabels.txt", "r") as my_file:
 
 # Streamlit interface
 st.title('HUMAN FRACTURE DETECTION')
-st.write('Choose detection method:')
 
-# Choose detection method
-option = st.selectbox('Select detection method', ('Image Upload'))
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-if option == 'Image Upload':
-    # Upload image through Streamlit
-    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+if uploaded_file is not None:
+    # Read the image
+    frame = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+    # Perform object detection on the frame
+    results = model.predict(frame)
+    detections = results[0].boxes.data
+    px = pd.DataFrame(detections).astype("float")
 
-    if uploaded_file is not None:
-        # Read the image
-        frame = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
-        # Perform object detection on the frame
-        results = model.predict(frame)
-        detections = results[0].boxes.data
-        px = pd.DataFrame(detections).astype("float")
+    # Display the detected objects and their class labels
+    for index, row in px.iterrows():
+        x1, y1, x2, y2, _, d = map(int, row)
+        c = class_list[d]
 
-        # Display the detected objects and their class labels
-        for index, row in px.iterrows():
-            x1, y1, x2, y2, _, d = map(int, row)
-            c = class_list[d]
+        # Draw bounding boxes and class labels on the frame
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.putText(frame, str(c), (x1, y1),
+                    cv2.FONT_HERSHEY_COMPLEX, 0.75, (255, 0, 0), 1)
 
-            # Draw bounding boxes and class labels on the frame
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            cv2.putText(frame, str(c), (x1, y1),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.75, (255, 0, 0), 1)
-
-        # Display the frame with objects detected
-        st.image(frame, channels="BGR", caption="Detected Objects", use_column_width=True)
+    # Display the frame with objects detected
+    st.image(frame, channels="BGR", caption="Detected Objects", use_column_width=True)
 
 
